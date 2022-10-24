ce4617f737df4136096c09b07afb73b508961c76
pablo oliveira <pabloliverfe@gmail.com>
2022-10-23 21:57:46 -0300

    mudado o banco para pabloliverfe

--- services/firebaseConn.js
+++ services/firebaseConn.js
@@ -2,13 +2,15 @@ const firebase = require('firebase');
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAwTCgK-u9nJ86jnKQKyZLdjXkiodqq4gw",
-    authDomain: "venturarefri-3ec83.firebaseapp.com",
-    projectId: "venturarefri-3ec83",
-    storageBucket: "venturarefri-3ec83.appspot.com",
-    messagingSenderId: "9844645214",
-    appId: "1:9844645214:web:bb80283b5ebe51f81083cf"
-  };
+  apiKey: "AIzaSyCuOiZuzPCpvpl1maB-pYc1Xxkp4nhRYTI",
+  authDomain: "sherenergy-5c04b.firebaseapp.com",
+  databaseURL: "https://sherenergy-5c04b-default-rtdb.firebaseio.com",
+  projectId: "sherenergy-5c04b",
+  storageBucket: "sherenergy-5c04b.appspot.com",
+  messagingSenderId: "1055108278134",
+  appId: "1:1055108278134:web:3426880d5e9fa85ed8fc57",
+  measurementId: "G-FF9MDHCCL7"
+};
 
 
 
