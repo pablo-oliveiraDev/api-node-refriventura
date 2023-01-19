3c0077f0241db1578d9b271e56a8fb1a5ce122d5
pablo <pabloliverfe@gmail.com>
2023-01-19 12:22:46 -0300

    loja 19-01-2023

--- dataBases/users.js
+++ dataBases/users.js
@@ -1,6 +1,8 @@
 const firebase = require('../services/firebaseConn');
 
+
 const db = firebase.firestore();
+
 const Users = db.collection('users');
 const Serv = db.collection('Servicos');
 module.exports = {Users:Users,Serv:Serv};
